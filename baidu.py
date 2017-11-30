bcdb669a0292058cba375e8b1b4304bfd12743f6
shangpf1 <208418427@qq.com>
2017-11-30 20:50:29 +0800

    作业01

---
+++ baidu.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+# for windows user
+# drvier = webdriver.Chrome(executable_path="./chromedriver.exe")
+# for mac user
+drvier = webdriver.Chrome(executable_path="./chromedriver")
+
+drvier.get("https://www.baidu.com/")
+baiduserach =  drvier.find_element_by_id("kw")
+
+baiduserach.send_keys("helloworld")
