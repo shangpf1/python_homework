dd5d42336777c7506b49a7fe7a902b6e156b439e
shangpf1 <208418427@qq.com>
2018-01-21 21:03:52 +0800

    自动关闭提示框

--- altertest.py
+++ altertest.py
@@ -1,4 +1,7 @@
+'''
+跳出删除提示框
 
+'''
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
 import time
@@ -8,6 +11,11 @@ driver = webdriver.Chrome()
 # 打开登录页面
 driver.get("file:///D:/Learn-python/python_homework/alert.html")
 
+# 点击按钮
 driver.find_element_by_tag_name('button').click()
+
+# 延时
 time.sleep(3)
+
+# 自动关闭提示框
 Alert(driver).accept()
