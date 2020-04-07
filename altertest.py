63d6799a8704e8c48065cc1dea307bb6abb0c020
shangpf1 <208418427@qq.com>
2020-04-07 11:22:51 +0800

    关闭Alert弹框

--- altertest.py
+++ altertest.py
@@ -2,20 +2,27 @@
 跳出删除提示框
 
 '''
+import os
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
 import time
 
+# 先查找到alert.html 的路径
+# dirname=os.path.dirname(os.path.dirname(__file__))
+# file=os.path.join(dirname,'alert.html')
+# print(file)
 
 driver = webdriver.Chrome()
+driver.maximize_window()
+
 # 打开登录页面
-driver.get("file:///D:/Learn-python/python_homework/alert.html")
+driver.get("file:///D:/python_homework/alert.html")
 
 # 点击按钮
 driver.find_element_by_tag_name('button').click()
 
 # 延时
-time.sleep(3)
+time.sleep(1)
 
 # 自动关闭提示框
 Alert(driver).accept()
