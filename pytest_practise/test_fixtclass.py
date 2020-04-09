989926edd4dfdd6f20ad71fb08d2bacb4a191492
shangpf1 <208418427@qq.com>
2020-04-09 15:51:26 +0800

    类和方法的运行顺序（setup/setup_class/setup_method等等）

--- pytest_practise/test_fixtclass.py
+++ pytest_practise/test_fixtclass.py
@@ -39,6 +39,7 @@ class TestCse():
 if __name__ == "__main__":
     pytest.main(["-s", "test_fixtclass.py"])
 
+# 备注：这里setup_method和teardown_method的功能和setup/teardown功能是一样的，一般二者用其中一个即可
 """
 运行结果：
 setup_class:所有用例执行前
@@ -49,7 +50,6 @@ teardown:每个用例结束后执行
 teardown_method:每个用例结束后执行
 
 setup_method:每个用例开始前执行
-
 setup:每个用例开始前执行
 PASSED                                [100%]正在执行----test_two
 teardown:每个用例结束后执行
